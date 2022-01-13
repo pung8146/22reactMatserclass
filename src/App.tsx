7820fa55bfa0061a9cf424fcbb5c45f7f726f4d6
pung8146 <pung8146@naver.com>
2022-01-13 13:28:14 +0900

    Typescript props부터element 요소까지

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import Circle from "./Circle";
+import React, {useState} from "react";
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.div`
+  color : ${(props) => props.theme.textColor};  
+`;
+
+interface DummyProps {
+  text: string;
+  otherThingHere?: boolean;
+}
+
+function Dummy ({text,otherThingHere}:DummyProps) {
+  return <H1>{text}</H1>
+}
 
 function App() {
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal"/>
-      <Circle text="hello Wrold" bgColor="tomato"/>
-    </div>
+    <Container>
+      <H1>protected</H1>
+      <Dummy text="Hello" />
+      <button onClick={onClick}>Button</button>
+    </Container>     
   );
-}
+  }
 
 export default App;
---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-component' {
+    export interface DefaultTheme {
+        textColor : string;
+        bgColor : string;
+        btnColor: string;    
+    };
+    }
