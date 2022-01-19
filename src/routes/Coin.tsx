caf19602ffbc133d3e4947bb708e4f8817c5e7d3
pung8146 <pung8146@naver.com>
2022-01-19 11:03:03 +0900

    react6 부터 Link to={ } state={} 로 방식이 변경되었음

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,46 @@
 import { useParams } from 'react-router';
+import styled from 'styled-components';
+import {useState} from 'react';
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height:10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+font-size:48px;
+    color:${props => props.theme.accentColor};
+`
+
+const Loader = styled.span`
+    text-align: center;
+    display:block;
+`;
 
 interface Params {
 coinId: String;
 }
 
 function Coin() {
-const { coinId } = useParams() as unknown as Params;
-console.log(coinId);
-return <h1>Coin:{ coinId }</h1>;
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams() as unknown as Params;
+  console.log(coinId);
+  return(
+  <Container>
+      <Header>
+        <Title>코인{coinId}</Title>
+      </Header>
+      { loading ? <Loader>Loading...</Loader>: null} 
+  </Container>
+  );
 }
+
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import { useEffect, useState } from "react";
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -20,9 +22,10 @@ const Coin = styled.li`
     margin-bottom: 10px;
     border-radius:15px;
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color .2s ease-in;
-        display: block;
     }
     &:hover{
         a{
@@ -35,50 +38,67 @@ const Title = styled.h1`
 font-size:48px;
     color:${props => props.theme.accentColor};
 `
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-        },
-];
+
+const Loader = styled.span`
+    text-align: center;
+    display:block;
+`;
+
+const Img = styled.img`
+    width:35px;
+    height:35px;
+    margin-right: 10px;
+`;
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
+interface RouterState {
+    name: string;
+    }
+    
 
 function Coins() {
+    const [coins,setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true); 
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0,100));
+            setLoading(false);
+        })();
+    }, []);
+    console.log(coins);
     return ( 
     <Container>
         <Header>
             <Title>코인</Title>
         </Header>
+        { loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
         <CoinsList>
-            {coins.map(coin => <Coin key={coin.id}>
-                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-            </Coin>)}
-        
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={{name:coin.name, rank:coin.rank}}>
+                  <Img 
+                    src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}` }
+                  />
+                  {coin.name} &rarr;
+                </Link>
+            </Coin>
+          ))}
         </CoinsList>
+      )}
     </Container>
-    );   
+  );
 }
-
 export default Coins;
