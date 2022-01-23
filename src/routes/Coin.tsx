ea7d6ac6e0fe8a4627b507977976d7727c260276
pung8146 <pung8146@naver.com>
2022-01-23 11:13:53 +0900

    Nested Routes 적용을 V6로 해보기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,9 @@
+import { Route, Routes } from 'react-router-dom';
 import { useLocation, useParams } from 'react-router';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -24,6 +27,29 @@ const Loader = styled.span`
     text-align: center;
     display:block;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 
 interface Params {
     coinId: String;
@@ -115,9 +141,45 @@ function Coin() {
     return (
         <Container>
             <Header>
-                <Title>{state?.name || "Loading..."}</Title>
+                <Title>
+                    {state?.name ? state.name : loading ? "Loading..." : info?.name}
+                </Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Routes>
+                        <Route path="chart" element={<Chart />} />
+                        <Route path="price" element={<Price />} />
+                    </Routes>
+                </>
+            )}
         </Container>
     );
 }
