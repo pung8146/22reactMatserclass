7cb160d3a109773f9c244fdd1fc719842dbeccd1
pung8146 <pung8146@naver.com>
2022-01-23 12:07:14 +0900

    useMatch / react-router-dom 에서 불러와서 사용했음 // 그러나 price char 정보가 안나옴

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
-import { BrowserRouter ,Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Coins from "./routes/Coins"
 import Coin from "./routes/Coin"
+import Chart from "./routes/Chart"
+import Price from "./routes/Price"
 
-function Router( ) {
+function Router() {
     return <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Coins/>}></Route>
-            <Route path="/:coinId" element={<Coin/>}></Route>
+            <Route path="/" element={<Coins />}></Route>
+            <Route path="/:coinId" element={<Coin />}></Route>
+            <Route path="chart" element={<Chart />} />
+            <Route path="price" element={<Price />} />
         </Routes>
     </BrowserRouter>
 }
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import { useLocation, useParams } from 'react-router';
+import { Route, Routes, useLocation, useParams, useMatch } from 'react-router-dom';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import Chart from "./Chart";
 import Price from "./Price";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -48,6 +48,28 @@ const OverviewItem = styled.div`
 `;
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+        props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `;
 
 
@@ -123,6 +145,8 @@ function Coin() {
     const { state } = useLocation() as RouterState;
     const [info, setInfo] = useState<InfoData>();
     const [priceInfo, setPriceInfo] = useState<PriceData>();
+    const priceMatch = useMatch("/coinId/price");
+    const chartMatch = useMatch("/coinId/chart");
     useEffect(() => {
         (async () => {
             const infoData = await (
@@ -174,13 +198,25 @@ function Coin() {
                             <span>{priceInfo?.max_supply}</span>
                         </OverviewItem>
                     </Overview>
+
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/Price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+
+
                     <Routes>
                         <Route path="chart" element={<Chart />} />
                         <Route path="price" element={<Price />} />
                     </Routes>
                 </>
-            )}
-        </Container>
+            )
+            }
+        </Container >
     );
 }
 
